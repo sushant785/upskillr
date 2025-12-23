7185cff62da2802d470474fcd8032217e29b7f14
Sushant <sushantnistane1@gmail.com>
2025-12-23 12:45:45 +0530

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-slate-900">
+        <h1 className="text-4xl font-bold text-cyan-400">
+          Tailwind with Vite is Working
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
